fde7442c73ec95da486d697b78e1bf46fa8f4806
bustyAI <oscar120646@gmail.com>
2024-03-04 19:54:43 -0800

    creating style classes

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -4,19 +4,24 @@ import { PiCat } from "react-icons/pi"
 
 const Navbar = () => {
     return (
-        <header className='w-full absolute z-10 py-12 px-12'>
-            <nav className='flex justify-between items-center'>
-                <PiCat className='w-14 h-14 transition duration-300 transform hover:scale-105 hover-rainbow' />
-                <div className='flex flex-row space-x-4'>
-                    <Link href={"https://github.com/bustyAI"}>
+        <header className='header'>
+
+            <nav className='nav'>
+                <PiCat className='nav__icon' />
+                <div className='nav_link__container'>
+                    <Link href={"#"}>
                         <p className='navbar__button'>About</p>
                     </Link>
-                    <Link href={"https://github.com/bustyAI"}>
+                    <Link href={"#"}>
                         <p className='navbar__button'>Contact</p>
+                    </Link>
+                    <Link href={"https://github.com/bustyAI"}>
+                        <p className='navbar__button'>Github</p>
                     </Link>
                 </div>
 
             </nav>
+
         </header>
     )
 }
